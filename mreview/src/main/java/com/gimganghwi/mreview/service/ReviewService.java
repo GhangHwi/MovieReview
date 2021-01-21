7e5de2d9f18ac4950d11b50eccaa953277dd1612
gimganghwi <gimganghwi@gmail.com>
2021-01-21 21:47:06 +0900

    영화 리뷰 등록 모달, 리뷰 리스트 추가

---
+++ mreview/src/main/java/com/gimganghwi/mreview/service/ReviewService.java
@@ -0,0 +1,50 @@
+package com.gimganghwi.mreview.service;
+
+
+import com.gimganghwi.mreview.dto.ReviewDTO;
+import com.gimganghwi.mreview.entity.Member;
+import com.gimganghwi.mreview.entity.Movie;
+import com.gimganghwi.mreview.entity.Review;
+
+import java.util.List;
+
+public interface ReviewService {
+
+    List<ReviewDTO> getListOMovie(Long mno);
+
+    Long register(ReviewDTO movieReviewDTO);
+
+    void modify(ReviewDTO movieReviewDTO);
+
+    void remove(Long reviewnum);
+
+    default Review dtoToEntity(ReviewDTO movieReviewDTO){
+
+        Review movieReview = Review.builder()
+                //.reviewnum(movieReviewDTO.getReviewnum())
+                .movie(Movie.builder().mno(movieReviewDTO.getMno()).build())
+                .member(Member.builder().mid(movieReviewDTO.getMid()).build())
+                .grade(movieReviewDTO.getGrade())
+                .text(movieReviewDTO.getText())
+                .build();
+
+        return movieReview;
+    }
+
+    default ReviewDTO entityToDto(Review movieReview){
+
+        ReviewDTO movieReviewDTO = ReviewDTO.builder()
+                .reviewnum(movieReview.getReviewnum())
+                .mno(movieReview.getMovie().getMno())
+                .mid(movieReview.getMember().getMid())
+                .nickname(movieReview.getMember().getNickname())
+                .email(movieReview.getMember().getEmail())
+                .grade(movieReview.getGrade())
+                .text(movieReview.getText())
+                .regDate(movieReview.getRegDate())
+                .modDate(movieReview.getModDate())
+                .build();
+
+        return movieReviewDTO;
+    }
+}
